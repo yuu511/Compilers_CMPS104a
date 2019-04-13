0ae03d1a26a9bfb82085c71ece314a146900f644
elijah <pipimi@tfwno.gf>
2019-04-12 17:49:00 -0700

    use leet strtok

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -25,12 +25,12 @@ constexpr size_t LINESIZE = 1024;
 
 // strip the file extension any string  
 string strp (char* filename){
-   string input_stripped = string(basename(filename));
-   size_t lastindex = input_stripped.find_last_of(".");
-   input_stripped = input_stripped.substr(0,lastindex);
-   char* token = strtok (filename, ".");
+   char* token = strtok (filename,".");
    printf("stripped token : %s", token);
-   return input_stripped;
+   // string input_stripped = string(basename(filename));
+   // size_t lastindex = input_stripped.find_last_of(".");
+   // input_stripped = input_stripped.substr(0,lastindex);
+   return string(token);
 }
 
 // Chomp the last character from a buffer if it is delim.
