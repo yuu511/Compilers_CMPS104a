70c5f234980e938531fd70f9464c907da1a4bab6
elijah <pipimi@tfwno.gf>
2019-06-22 03:30:55 -0700

    begin

--- lab5/3ac.cpp
+++ lab5/3ac.cpp
@@ -1,7 +1,10 @@
 #include "3ac.h"
 #include "symbol_table.h"
 #include "astree.h"
+#include "lyutils.h"
 #include "auxlib.h"
+
+vector<string> *all_strings  = new vector <string>();
 
 string parse_typesize(symbol *sym){
   attr_bitset a = sym->attributes;
@@ -51,7 +54,56 @@ void ac_struct(symbol_table *struct_table,FILE *out){
   }
 }
 
-void emit_3ac(astree *s, symbol_table *struct_table, 
-  unordered_map<const string*,symbol_table*> *master,FILE *out){
+void ac_globalvar(astree *child, unordered_map<const string*, symbol_table*> *master, FILE *out){
+  for (size_t i =0; i< child->attributes.size(); i++){
+    if (child->attributes[i]){
+      switch(i){
+        case static_cast<int>(attr::INT):
+          if (child->children.size() > 2 ){
+            //parse the expr
+            if (child->children[2]->children.size() == 0){
+              fprintf (out,"%s: .global int %s\n",
+                       child->children[1]->lexinfo->c_str(),
+                       child->children[2]->lexinfo->c_str());
+            }
+            else {
+              errprintf ("global variable may not have non-static value assigned to it\n");
+            }
+          }
+          else {
+            fprintf (out,"%s: .global int\n",child->children[1]->lexinfo->c_str());
+          }
+          break;
+        case static_cast<int>(attr::STRING):
+          if (child->children.size() > 2 ){
+
+          } else {
+            
+          }
+          break;
+      }
+    }
+  }
+}
+
+void ac_traverse(astree *s, unordered_map<const string*, symbol_table*> *master, FILE *out){
+  if ( s == nullptr || master == nullptr || out == nullptr ){
+    errprintf("ac_traverse called on uninitialized structure!\n");
+    return;
+  }
+  for (astree *child: s->children){
+    if (child->symbol == TOK_TYPE_ID){
+      // process the global variable
+      ac_globalvar(child,master,out);
+    }
+    else {
+      // else process the function
+    }
+  }
+  // emit string constants here
+}
+
+void emit_3ac(astree *s, symbol_table *struct_table, unordered_map<const string*,symbol_table*> *master,FILE *out){
   ac_struct(struct_table,out);
+  ac_traverse(s,master,out);
 }
